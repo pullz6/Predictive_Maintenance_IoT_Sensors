7b3e4d2de59a90955f37f8585dc73c23f1197731
Sara <gunawardhanapulsara@gmail.com>
2025-10-21 17:58:03 +0100

    eda and prep starting

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -3,7 +3,16 @@ import numpy as np
 
 def read_data(path): 
     df = pd.read_csv(path)
-    return df 
+    return df
+
+def fill_nulls(df): 
+    df = df.fillna(df.mean(numeric_only=True))
+    return df
+
+def check_datatypes(): 
+    '''Checking if all the data types are matching'''
+    check = {'UDI': int, 'Product ID': string , 'Type': string, }
+    return 'True'
 
 df = read_data('data/ai4i2020.csv')
 print(df.info())
