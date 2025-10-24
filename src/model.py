c3a5dcd3acf43e875af967c93cd78f45521689a7
Sara <gunawardhanapulsara@gmail.com>
2025-10-24 16:39:01 +0100

    adding a readme file

--- src/model.py
+++ src/model.py
@@ -3,7 +3,6 @@ Production MLflow Training =
 Features: Experiment tracking, model registry, reproducibility, testing, CI/CD readiness
 """
 
-
 import mlflow
 import logging
 import warnings
@@ -13,7 +12,8 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 class MLflowProductionTrainer:
-    
+    def __init__(self,experiement_name:str, tracking_uri: Optional[str] = None):
+        pass
     
 def mlflow_model_training (): 
     # Set our tracking server uri for logging
