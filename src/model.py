9d4d7b3f22f44720bbe3f67d6848ca67888e4f6d
Sara <gunawardhanapulsara@gmail.com>
2025-12-31 14:29:41 +0000

    model evaluation

--- src/model.py
+++ src/model.py
@@ -27,56 +27,60 @@ logger = logging.getLogger(__name__)
 class MLflowProductionTrainer:
     def __init__(self, config: Dict[str, Any]):
         self.config = config 
-        #self.set_up_mlflow()
-    
-    def set_up_mlflow(self):
-        """Setup MLflow tracking and registry"""
-        logger.info("Setting up Mlflow")
-        # MLflow Tracking
-        mlflow.set_tracking_uri(self.config['mlflow']['tracking_uri'])
-        mlflow.set_experiment(self.config['mlflow']['experiment_name'])
-        
-        # MLflow Client
-        self.mlflow_client = mlflow.MlflowClient()
-        
-        # Enable autologging
-        mlflow.sklearn.autolog()
   
-        
     def building_model(self):
         logger.info("Building Model")
         model = XGBClassifier()
         logger.info("Building Model - Success")
         return model
     
-        
-    def training_model(self): 
+    def load_split_data(self): 
         logger.info("Reading data")
         X = pd.read_parquet("data/processed/X.parquet")
         y = pd.read_parquet("data/processed/y.parquet")
         logger.info("Reading data - Success")
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+        X_train, X_test,y_train, y_test = train_test_split(X, y, test_size=0.3)
+        return X_train, X_test,y_train, y_test
+    
+    def training_model(self): 
+        X_train, X_test,y_train, y_test = self.load_split_data()
         logger.info("Training Model")
         model = self.building_model()
         model.fit(X_train, y_train)
         logger.info("Training Model - Success")
+        return model
     
-     # def evaluating_model(): 
-     
-     # def upload_artifact():
+    def evaluating_model(self): 
+        model = self.training_model()
+        X_train, X_test,y_train, y_test = self.load_split_data()
+        y_pred = model.predict(X_test)
+        model_acc = accuracy_score(y_test, y_pred)
+        return model_acc
       
     def run_mlops_pipeline(self):
         """Complete MLOps pipeline"""
         logger.info("Starting MLOps pipeline...")
-        
+        try:
+            exp_name = self.config['mlflow']['experiment_name']
+        except KeyError as e:
+            print(f"Missing config key: {e}")
+            exp_name = 'Sensor_Prediction'
+        mlflow.set_experiment(exp_name)
         with mlflow.start_run(run_name=f"mlops-{datetime.now().strftime('%Y%m%d-%H%M%S')}") as run:
-            self.building_model()
-            self.training_model()
+            model = self.training_model()
+            model_info = mlflow.xgboost.log_model(
+                        xgb_model=model,
+                        name="xgboost_model",
+            )
+            accuracy = self.evaluating_model()
+            mlflow.log_metric("accuracy", accuracy)
 
 
 if __name__ == "__main__": 
     # Configuration of the class 
-    CONFIG = {"environment": "production",
+    CONFIG = {
+        
+    "environment": "production",
     
     "mlflow": {
         "tracking_uri": os.environ.get('MLFLOW_TRACKING_URI', 'http://localhost:5000'),  
